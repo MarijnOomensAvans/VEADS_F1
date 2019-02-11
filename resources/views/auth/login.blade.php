02fca8b7cd9d26dfd34020afdfa93eb659a3061d
Daan van Berkel <info@daanvanberkel.nl>
2019-02-11 18:36:25 +0100

    Register disabled, changed login page to match theme

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,76 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,400i,600,700">
+    <link rel="stylesheet" id="css-main" href="{{ asset('css/dashmix.min-1.5.css') }}">
+</head>
+<body>
+<div id="page-container" class="side-trans-enabled">
+    <main id="main-container">
+        <div class="row no-gutters bg-primary-op">
+            <div class="hero-static col-md-6 d-flex align-items-center bg-white">
+                <div class="p-3 w-100">
+                    <div class="mb-3 text-center">
+                        <a class="link-fx font-w700 font-size-h1" href="{{ route('home') }}">
+                            VEADS
+                        </a>
+                        <p class="text-uppercase font-w700 font-size-sm text-muted">Inloggen</p>
+                    </div>
+                    <div class="row no-gutters justify-content-center">
+                        <div class="col-sm-8 col-xl-6">
+                            <form action="{{ route('login') }}" method="POST">
+                                @csrf
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
+                                <div class="py-3">
+                                    <div class="form-group">
+                                        <input type="email" class="form-control form-control-lg form-control-alt{{ $errors->has('email') ? ' is-invalid' : '' }}" value="{{ old('email') }}" id="email" name="email" required placeholder="E-mailadres">
+                                        @if ($errors->has('email'))
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $errors->first('email') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="password" class="form-control form-control-lg form-control-alt{{ $errors->has('password') ? ' is-invalid' : '' }}" id="password" name="password" required placeholder="Wachtwoord">
+                                        @if ($errors->has('password'))
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $errors->first('password') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
                                 </div>
-                            </div>
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-block btn-hero-lg btn-hero-primary">
+                                        <i class="fa fa-fw fa-sign-in-alt mr-1"></i> Inloggen
+                                    </button>
+                                    @if (Route::has('password.request'))
+                                        <p class="mt-3 mb-0 d-lg-flex justify-content-lg-between">
+                                            <a class="btn btn-sm btn-light d-block d-lg-inline-block mb-1" href="{{ route('password.request') }}">
+                                                <i class="fa fa-exclamation-triangle text-muted mr-1"></i> Wachtwoord vergeten
+                                            </a>
+                                        </p>
+                                    @endif
+                                </div>
+                            </form>
                         </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+                    </div>
+                </div>
+            </div>
+            <div class="hero-static col-md-6 d-none d-md-flex align-items-md-center justify-content-md-center text-md-center">
+                <div class="p-3">
+                    <p class="display-4 font-w700 text-white mb-3">
+                        Voor Elkaar Aan De Slag
+                    </p>
                 </div>
             </div>
         </div>
-    </div>
+    </main>
 </div>
-@endsection
+<script src="{{ asset('js/dashmix.core.min-1.5.js') }}"></script>
+<script src="{{ asset('js/dashmix.app.min-1.5.js') }}"></script>
+</body>
+</html>
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,65 +1,84 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,400i,600,700">
+    <link rel="stylesheet" id="css-main" href="{{ asset('css/dashmix.min-1.5.css') }}">
+</head>
+<body>
+<div id="page-container" class="side-trans-enabled">
+    <main id="main-container">
+        <div class="row no-gutters bg-gd-sun-op">
+            <div class="hero-static col-md-6 d-flex align-items-center bg-white">
+                <div class="p-3 w-100">
+                    <div class="text-center">
+                        <a class="link-fx text-warning font-w700 font-size-h1" href="{{ route('home') }}">
+                            VEADS
+                        </a>
+                        <p class="text-uppercase font-w700 font-size-sm text-muted">Wachtwoord vergeten</p>
+                    </div>
+                    <div class="row no-gutters justify-content-center">
+                        <div class="col-sm-8 col-xl-6">
+                            @if (session('status'))
+                                <div class="alert alert-success" role="alert">
+                                    {{ session('status') }}
+                                </div>
+                            @endif
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+                                <form method="POST" action="{{ route('password.update') }}">
+                                    @csrf
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('password.update') }}">
-                        @csrf
+                                    <input type="hidden" name="token" value="{{ $token }}">
 
-                        <input type="hidden" name="token" value="{{ $token }}">
+                                    <div class="form-group py-3">
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                                        <input id="email" type="email" class="form-control form-control-lg form-control-alt{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ $email ?? old('email') }}" required autofocus placeholder="E-mailadres">
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ $email ?? old('email') }}" required autofocus>
+                                        @if ($errors->has('email'))
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $errors->first('email') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
 
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                                    <div class="form-group">
+                                            <input id="password" type="password" class="form-control form-control-lg form-control-alt{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required placeholder="Wachtwoord">
+
+                                            @if ($errors->has('password'))
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $errors->first('password') }}</strong>
+                                                </span>
+                                            @endif
+                                    </div>
+
+                                    <div class="form-group">
+                                            <input id="password-confirm" type="password" class="form-control form-control-lg form-control-alt" name="password_confirmation" required placeholder="Wachtwoord bevestiging">
+                                    </div>
+
+                                    <div class="form-group mb-0">
+                                        <button type="submit" class="btn btn-block btn-hero-lg btn-hero-warning">
+                                            <i class="fa fa-fw fa-reply mr-1"></i> Wachtwoord resetten
+                                        </button>
+                                    </div>
+                                </form>
                         </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Reset Password') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                    </div>
+                </div>
+            </div>
+            <div class="hero-static col-md-6 d-none d-md-flex align-items-md-center justify-content-md-center text-md-center">
+                <div class="p-3">
+                    <p class="display-4 font-w700 text-white mb-0">
+                        Voor Elkaar Aan De Slag
+                    </p>
                 </div>
             </div>
         </div>
-    </div>
+    </main>
 </div>
-@endsection
+<script src="{{ asset('js/dashmix.core.min-1.5.js') }}"></script>
+<script src="{{ asset('js/dashmix.app.min-1.5.js') }}"></script>
+</body>
+</html>
