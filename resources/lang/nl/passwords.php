c198170d339744d4ed028e6dd309925ff4c6e27e
Daan van Berkel <info@daanvanberkel.nl>
2019-02-21 13:03:30 +0100

    Validation error messages vertaald naar het nederlands

---
+++ resources/lang/nl/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Wachtwoorden moeten minimaal 6 tekens lang zijn en overeenkomen met de verificatie.',
+    'reset' => 'Je wachtwoord is gereset!',
+    'sent' => 'We hebben je een reset-link ge-e-maild!',
+    'token' => 'Deze wachtwoord reset-link is niet (meer) geldig.',
+    'user' => "Er bestaat geen gebruiker met dit e-mailadres.",
+
+];
