d5a9775650a85e2b2396b241df3eac2406a9b42d
Glenn van Alphen <gpm.vanalphen@student.avans.nl>
2019-02-25 01:39:31 +0100

    Add volunteers table

---
+++ database/migrations/2019_02_25_002933_create_volunteers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVolunteersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('volunteers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('first_name', 50);
+            $table->string('last_name', 50);
+            $table->string('phone_number', 50);
+            $table->unsignedInteger('address_id');
+            $table->timestamps();
+
+            $table->foreign('address_id')->references('id')->on('addresses');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('volunteers');
+    }
+}
