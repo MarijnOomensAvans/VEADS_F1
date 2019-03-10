500cf8965c41c48644037505d68cc28a32aea452
Marijn <mdoomens@avans.nl>
2019-03-10 11:12:54 +0100

    Plaful

--- resources/views/front/master.blade.php
+++ resources/views/front/master.blade.php
@@ -82,11 +82,9 @@
                             <div class="display-table-cell vertical-align-middle">
                                 <a href="/"><img class="footer-logo" src="/images/logo.png" alt="Logo"></a>
                                 <div class="social-icon-style-8">
-                                    <ul class="text-extra-small margin-20px-top xs-no-margin-bottom text-uppercase no-padding no-margin-bottom list-style-none">
-                                        <li class="display-inline-block margin-10px-right"><a href="http://twitter.com" target="_blank" title="Twitter">Twitter</a></li>
-                                        <li class="display-inline-block margin-10px-right"><a href="http://facebook.com" target="_blank" title="Facebook">Facebook</a></li>
-                                        <li class="display-inline-block margin-10px-right"><a href="http://instagram.com" target="_blank" title="Instagram">Instagram</a></li>
-                                    </ul>
+                                <ul class="text-extra-small margin-20px-top xs-no-margin-bottom text-uppercase no-padding no-margin-bottom list-style-none bigger-font">
+                                        <li class="display-inline-block margin-10px-right"><a class="facebook text-white" href="https://www.facebook.com/socialreturnworks/" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+                                        <li class="display-inline-block margin-10px-right"><a class="instagram text-white" href="https://www.instagram.com/voorelkaaraandeslag/" target="_blank"><i class="fab fa-instagram"></i></a></li>
                                 </div>
                             </div>
                         </div>
