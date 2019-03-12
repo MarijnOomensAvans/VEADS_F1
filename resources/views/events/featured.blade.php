65abc9d1c05ceb15eb559cef39c84f3ac46f9a03
Daan van Berkel <info@daanvanberkel.nl>
2019-03-12 10:40:53 +0100

    Events worden nu alleen weergegeven als deze gepubliceerd zijn, ook de uitgelichte evenenemten En ik heb een bug opgelost met de foto in de banner. Als er geen foto's bij het event zijn toegevoegd wordt nu dezelfde foto als in de homepage banner weergegeven als placeholder

--- resources/views/events/featured.blade.php
+++ resources/views/events/featured.blade.php
@@ -10,53 +10,23 @@
 
         <div class="content">
             <div class="row">
-                <div class="col-12 col-md-4">
-                    <div class="block block-rounded block-bordered">
-                        <div class="block-content">
-                            @if(isset($events[0]))
-                                <h2><a href="{{ route('admin/event', ['event' => $events[0]]) }}">{{ $events[0]->name }}</a></h2>
-                            @else
-                                <h2>Selecteer een evenement</h2>
-                            @endif
+                @for($i = 0; $i < 3; $i++)
+                    <div class="col-12 col-md-4">
+                        <div class="block block-rounded block-bordered">
+                            <div class="block-content">
+                                @if(isset($events[$i]))
+                                    <h2><a href="{{ route('admin/event', ['event' => $events[$i]]) }}">{{ $events[$i]->name }}</a></h2>
+                                @else
+                                    <h2>Selecteer een evenement</h2>
+                                @endif
 
-                            <div class="mb-3">
-                                <event-search-component position="1"{{ isset($events[0]) ? 'event="' . $events[0]->id . '"' : '' }}></event-search-component>
+                                <div class="mb-3">
+                                    <event-search-component position="{{ $i + 1 }}" published="1"{{ isset($events[$i]) ? ' event="' . $events[$i]->id . '"' : '' }}></event-search-component>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-
-                <div class="col-12 col-md-4">
-                    <div class="block block-rounded block-bordered">
-                        <div class="block-content">
-                            @if(isset($events[1]))
-                                <h2><a href="{{ route('admin/event', ['event' => $events[1]]) }}">{{ $events[1]->name }}</a></h2>
-                            @else
-                                <h2>Selecteer een evenement</h2>
-                            @endif
-
-                            <div class="mb-3">
-                                <event-search-component position="2"{{ isset($events[1]) ? 'event="' . $events[1]->id . '"' : '' }}></event-search-component>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-12 col-md-4">
-                    <div class="block block-rounded block-bordered">
-                        <div class="block-content">
-                            @if(isset($events[2]))
-                                <h2><a href="{{ route('admin/event', ['event' => $events[2]]) }}">{{ $events[2]->name }}</a></h2>
-                            @else
-                                <h2>Selecteer een evenement</h2>
-                            @endif
-
-                            <div class="mb-3">
-                                <event-search-component position="3"{{ isset($events[2]) ? 'event="' . $events[2]->id . '"' : '' }}></event-search-component>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                @endfor
             </div>
 
             <div class="row">
