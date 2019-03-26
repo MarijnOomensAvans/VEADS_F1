70f1378367767fcd2f38a3c51c28947663fbd97f
Daan van Berkel <info@daanvanberkel.nl>
2019-03-26 11:32:39 +0100

    Forms zijn nu meer IDPAT proof

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -1,13 +1,5 @@
 @extends('layouts.admin')
 
-@php
-$labelSmWidth = 4;
-$labelLgWidth = 3;
-
-$inputSmWidth = 12 - $labelSmWidth;
-$inputLgWidth = 12 - $labelLgWidth;
-@endphp
-
 @section('content')
     <div class="content">
         @if(isset($event))
@@ -24,42 +16,31 @@ $inputLgWidth = 12 - $labelLgWidth;
                     <div class="col-12">
                         <form method="post" action="{{ (isset($event) ? route('admin/events/edit', ['event' => $event]) : route('admin/events/create')) }}" enctype="multipart/form-data">
                             @csrf
-                            <div class="form-group row">
-                                <label class="col-sm-{{ $labelSmWidth }} col-lg-{{ $labelLgWidth }} col-form-label" for="name">Evenement naam</label>
-                                <div class="col-sm-{{ $inputSmWidth }} col-lg-{{ $inputLgWidth }}">
-                                    <input type="text" name="name" id="name" class="form-control{{ ($errors->has('name') ? ' is-invalid' : '') }}" value="{{ old('name', $event->name ?? '') }}" placeholder="Evenement naam" />
-                                    @if($errors->has('name'))
-                                        <div class="invalid-feedback">
-                                            {{ $errors->first('name') }}
-                                        </div>
-                                    @endif
-                                </div>
-                            </div>
+
+                            @component('includes.forms.formgroup', [
+                                'name' => 'name',
+                                'title' => 'Evenement naam',
+                                'prefill' => $event->name ?? ''
+                            ])@endcomponent
+
+                            @component('includes.forms.wysiwyg', [
+                                'name' => 'description',
+                                'title' => 'Evenement omschrijving',
+                                'prefill' => $event->description ?? ''
+                            ])@endcomponent
 
                             <div class="form-group row mb-5">
-                                <label class="col-sm-{{ $labelSmWidth }} col-lg-{{ $labelLgWidth }} col-form-label" for="description">Evenement omschrijving</label>
-                                <div class="col-sm-{{ $inputSmWidth }} col-lg-{{ $inputLgWidth }}">
-                                    <textarea class="form-control{{ ($errors->has('description') ? ' is-invalid' : '') }}" name="description" id="description" rows="15" placeholder="Evenement omschrijving">{{ old('description', $event->description ?? '') }}</textarea>
-                                    @if($errors->has('description'))
-                                        <div class="invalid-feedback">
-                                            {{ $errors->first('description') }}
-                                        </div>
-                                    @endif
-                                </div>
-                            </div>
-
-                            <div class="form-group row mb-5">
-                                <label class="col-sm-{{ $labelSmWidth }} col-lg-{{ $labelLgWidth }} col-form-label" for="name">Project</label>
-                                <div class="col-sm-{{ $inputSmWidth }} col-lg-{{ $inputLgWidth }}">
+                                <label class="col-sm-4 col-lg-3 col-form-label text-sm-right" for="name">Project</label>
+                                <div class="col-sm-8 col-lg-9">
                                     <project-search-component project="{{ old('project_id', $event->project_id ?? '') }}"></project-search-component>
                                 </div>
                             </div>
 
                             <div class="form-group row mb-5">
-                                <label class="col-sm-{{ $labelSmWidth }} col-lg-{{ $labelLgWidth }} col-form-label" for="price">Entree prijs
+                                <label class="col-sm-4 col-lg-3 col-form-label text-sm-right" for="price">Entree prijs
                                     <span class="fas fa-question-circle" data-toggle="tooltip" data-placement="top" title="Dit wordt alleen op de website weergegeven als de prijs groter is dan 0."></span>
                                 </label>
-                                <div class="input-group col-sm-{{ $inputSmWidth }} col-lg-{{ $inputLgWidth }}">
+                                <div class="input-group col-sm-8 col-lg-9">
                                     <div class="input-group-prepend">
                                         <span class="input-group-text">
                                             <i class="fa fa-euro-sign"></i>
@@ -75,8 +56,8 @@ $inputLgWidth = 12 - $labelLgWidth;
                             </div>
 
                             <div class="form-group row">
-                                <label class="col-sm-{{ $labelSmWidth }} col-lg-{{ $labelLgWidth }} col-form-label" for="start_date">Begindatum/-tijd</label>
-                                <div class="col-sm-{{ $inputSmWidth / 2 }} col-lg-6">
+                                <label class="col-sm-4 col-lg-3 col-form-label text-sm-right" for="start_date">Begindatum/-tijd</label>
+                                <div class="col-sm-4 col-lg-6">
                                     <input type="date" name="start_date" id="start_date" class="form-control{{ ($errors->has('start_datetime') ? ' is-invalid' : '') }}" value="{{ old('start_date', (isset($event) && !empty($event->datetime) && !empty($event->datetime->start) ? $event->datetime->start->format('Y-m-d') : '')) }}" placeholder="dd-mm-jjjj" />
                                     @if($errors->has('start_datetime'))
                                         <div class="invalid-feedback">
@@ -84,7 +65,7 @@ $inputLgWidth = 12 - $labelLgWidth;
                                         </div>
                                     @endif
                                 </div>
-                                <div class="col-sm-{{ $inputSmWidth / 2 }} col-lg-3">
+                                <div class="col-sm-4 col-lg-3">
                                     <input type="time" name="start_time" id="start_time" class="form-control{{ ($errors->has('start_datetime') ? ' is-invalid' : '') }}" value="{{ old('start_time', (isset($event) && !empty($event->datetime) && !empty($event->datetime->start) ? $event->datetime->start->format('H:i') : '')) }}" placeholder="uu:mm" />
                                     @if($errors->has('start_datetime'))
                                         <div class="invalid-feedback">
@@ -95,8 +76,8 @@ $inputLgWidth = 12 - $labelLgWidth;
                             </div>
 
                             <div class="form-group row mb-5">
-                                <label class="col-sm-{{ $labelSmWidth }} col-lg-{{ $labelLgWidth }} col-form-label" for="end_date">Einddatum/-tijd</label>
-                                <div class="col-sm-{{ $inputSmWidth / 2 }} col-lg-6">
+                                <label class="col-sm-4 col-lg-3 col-form-label text-sm-right" for="end_date">Einddatum/-tijd</label>
+                                <div class="col-sm-4 col-lg-6">
                                     <input type="date" name="end_date" id="end_date" class="form-control{{ ($errors->has('end_datetime') ? ' is-invalid' : '') }}" value="{{ old('end_date', (isset($event) && !empty($event->datetime) && !empty($event->datetime->end) ? $event->datetime->end->format('Y-m-d') : '')) }}" placeholder="dd-mm-jjjj" />
                                     @if($errors->has('end_datetime'))
                                         <div class="invalid-feedback">
@@ -104,7 +85,7 @@ $inputLgWidth = 12 - $labelLgWidth;
                                         </div>
                                     @endif
                                 </div>
-                                <div class="col-sm-{{ $inputSmWidth / 2 }} col-lg-3">
+                                <div class="col-sm-4 col-lg-3">
                                     <input type="time" name="end_time" id="end_time" class="form-control{{ ($errors->has('end_datetime') ? ' is-invalid' : '') }}" value="{{ old('end_time', (isset($event) && !empty($event->datetime) && !empty($event->datetime->end) ? $event->datetime->end->format('H:i') : '')) }}" placeholder="uu:mm" />
                                     @if($errors->has('end_datetime'))
                                         <div class="invalid-feedback">
@@ -117,8 +98,8 @@ $inputLgWidth = 12 - $labelLgWidth;
                             @include('includes.forms.address', ['address' => (isset($event) ? $event->address: null)])
 
                             <div class="form-group row mb-5">
-                                <label class="col-sm-{{ $labelSmWidth }} col-lg-{{ $labelLgWidth }} col-form-label" for="image">Foto's</label>
-                                <div class="col-sm-{{ $inputSmWidth / 2 }} col-lg-{{ $inputLgWidth }}">
+                                <label class="col-sm-4 col-lg-3 col-form-label text-sm-right" for="image">Foto's</label>
+                                <div class="col-sm-8 col-lg-9">
                                     <input type="file" name="image[]" id="image" accept="image/jpeg,image/jpg,image/png,image/png,image/svg" multiple/>
 
                                     @if(isset($event) && count($event->pictures))
@@ -130,7 +111,7 @@ $inputLgWidth = 12 - $labelLgWidth;
                             </div>
 
                             <div class="form-group row mb-5">
-                                <div class="offset-sm-{{ $labelSmWidth }} offset-lg-{{ $labelLgWidth }} col-sm-{{ $inputSmWidth / 2 }} col-lg-{{ $inputLgWidth }}">
+                                <div class="offset-sm-4 offset-lg-3 col-sm-8 col-lg-9">
                                     <div class="custom-control custom-switch custom-control-lg mb-2">
                                         <input type="checkbox" class="custom-control-input" id="published" name="published" {{ isset($event) && $event->published ? "checked" : '' }} value="1">
                                         <label class="custom-control-label" for="published">Gepubliceerd</label>
@@ -151,26 +132,4 @@ $inputLgWidth = 12 - $labelLgWidth;
             </div>
         </div>
     </div>
-@endsection
-
-@section('scripts')
-    <link rel="stylesheet" type="text/css" href="{{ asset('js/plugins/summernote/summernote-bs4.css') }}" />
-    <script src="{{ asset('js/plugins/summernote/summernote-bs4.min.js') }}"></script>
-    <script src="{{ asset('js/plugins/summernote/summernote-nl-NL.js') }}"></script>
-    <script>
-        document.addEventListener('DOMContentLoaded', () => {
-            $(document).tooltip({ selector: '[data-toggle="tooltip"]' });
-            $('#description').summernote({
-                lang: 'nl-NL',
-                height: 300,
-                toolbar: [
-                    ['style', ['bold', 'italic', 'underline', 'clear']],
-                    ['fontsize', ['fontsize']],
-                    ['para', ['ol', 'ul']],
-                    ['misc', ['undo', 'redo']]
-                ],
-                placeholder: 'Evenement omschrijving'
-            });
-        });
-    </script>
 @endsection
--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,13 +1,5 @@
 @extends('layouts.admin')
 
-@php
-$labelSmWidth = 4;
-$labelLgWidth = 3;
-
-$inputSmWidth = 12 - $labelSmWidth;
-$inputLgWidth = 12 - $labelLgWidth;
-@endphp
-
 @section('content')
     <div class="content">
         @if(isset($project))
@@ -24,29 +16,18 @@ $inputLgWidth = 12 - $labelLgWidth;
                     <div class="col-12">
                         <form method="post" action="{{ (isset($project) ? route('admin/projects/edit', ['project' => $project]) : route('admin/projects/create')) }}">
                             @csrf
-                            <div class="form-group row">
-                                <label class="col-sm-{{ $labelSmWidth }} col-lg-{{ $labelLgWidth }} col-form-label" for="name">Project naam</label>
-                                <div class="col-sm-{{ $inputSmWidth }} col-lg-{{ $inputLgWidth }}">
-                                    <input type="text" name="name" id="name" class="form-control{{ ($errors->has('name') ? ' is-invalid' : '') }}" value="{{ old('name', $project->name ?? '') }}" placeholder="Project naam" />
-                                    @if($errors->has('name'))
-                                        <div class="invalid-feedback">
-                                            {{ $errors->first('name') }}
-                                        </div>
-                                    @endif
-                                </div>
-                            </div>
 
-                            <div class="form-group row mb-5">
-                                <label class="col-sm-{{ $labelSmWidth }} col-lg-{{ $labelLgWidth }} col-form-label" for="description">Project omschrijving</label>
-                                <div class="col-sm-{{ $inputSmWidth }} col-lg-{{ $inputLgWidth }}">
-                                    <textarea class="form-control{{ ($errors->has('description') ? ' is-invalid' : '') }}" name="description" id="description" rows="15" placeholder="Project omschrijving">{{ old('description', $project->description ?? '') }}</textarea>
-                                    @if($errors->has('description'))
-                                        <div class="invalid-feedback">
-                                            {{ $errors->first('description') }}
-                                        </div>
-                                    @endif
-                                </div>
-                            </div>
+                            @component('includes.forms.formgroup', [
+                                'name' => 'name',
+                                'title' => 'Project naam',
+                                'prefill' => $project->name ?? ''
+                            ])@endcomponent
+
+                            @component('includes.forms.wysiwyg', [
+                                'name' => 'description',
+                                'title' => 'Project omschrijving',
+                                'prefill' => $project->description ?? ''
+                            ])@endcomponent
 
                             @include('includes.forms.address', ['address' => (isset($project) ? $project->address: null)])
 
@@ -62,26 +43,4 @@ $inputLgWidth = 12 - $labelLgWidth;
             </div>
         </div>
     </div>
-@endsection
-
-@section('scripts')
-    <link rel="stylesheet" type="text/css" href="{{ asset('js/plugins/summernote/summernote-bs4.css') }}" />
-    <script src="{{ asset('js/plugins/summernote/summernote-bs4.min.js') }}"></script>
-    <script src="{{ asset('js/plugins/summernote/summernote-nl-NL.js') }}"></script>
-    <script>
-        document.addEventListener('DOMContentLoaded', () => {
-            $(document).tooltip({ selector: '[data-toggle="tooltip"]' });
-            $('#description').summernote({
-                lang: 'nl-NL',
-                height: 300,
-                toolbar: [
-                    ['style', ['bold', 'italic', 'underline', 'clear']],
-                    ['fontsize', ['fontsize']],
-                    ['para', ['ol', 'ul']],
-                    ['misc', ['undo', 'redo']]
-                ],
-                placeholder: 'Project omschrijving'
-            });
-        });
-    </script>
 @endsection
