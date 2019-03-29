0af10d51ae832a9790cf7f6223c5899388883c2f
juraj336 <jgrietbe@avans.nl>
2019-03-29 12:03:41 +0100

    TempVideoFix

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -48,8 +48,8 @@
 
 <!-- Start video -->
 <section>
-  <div class="embed-responsive embed-responsive-16by9">
-<iframe width="560" height="315" src="https://www.youtube.com/embed/qpDNXX1Gm-Y" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  <div>
+<iframe width="50%" height="315" src="https://www.youtube.com/embed/qpDNXX1Gm-Y" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </div>
 </section>
 
