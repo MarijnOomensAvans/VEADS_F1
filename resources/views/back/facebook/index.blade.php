2fedf39ff8be86392e67c74937678542df996a55
Bader Ali <>
2019-04-03 12:03:01 +0200

    added pages list

--- resources/views/back/facebook/index.blade.php
+++ resources/views/back/facebook/index.blade.php
@@ -8,9 +8,23 @@
     <div class="content">
         <div class="block block-rounded block-bordered">
             <div class="block-content mb-4">
-                <div class="row">
-                    <div class="col-12 col-md-7">
+                <div class="row mb-3">
+                    <div class="col-12 col-md-12">
                         <a href="{{$fb_login_url}}" class="btn btn-primary">Verbinden met Facebook</a>
+                    </div>
+                </div>
+
+                <hr/>
+
+                <div class="row">
+                    <div class="col-12 col-md-12">
+                        <h5>Verbonden Pages</h5>
+
+                        <ul>
+                            <li>Page #1</li>
+                            <li>Page #2</li>
+                            <li>Page #3</li>
+                        </ul>
                     </div>
                 </div>
             </div>
