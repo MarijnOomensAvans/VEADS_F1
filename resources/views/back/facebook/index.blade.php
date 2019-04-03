b01818e5196d3966870a34f18dc5cd473068c42e
Bader Ali <>
2019-04-03 11:50:22 +0200

    added layout

--- resources/views/back/facebook/index.blade.php
+++ resources/views/back/facebook/index.blade.php
@@ -1,1 +1,19 @@
-Hallo
+@extends('layouts.admin')
+
+@section('content')
+    <div class="content">
+        <h1>Facebook</h1>
+    </div>
+
+    <div class="content">
+        <div class="block block-rounded block-bordered">
+            <div class="block-content">
+                <div class="row">
+                    <div class="col-12 col-md-7">
+                        <a href="https://facebook.com" class="btn btn-primary"><span class="fas fa-plus"></span> Evenement toevoegen</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
