7ddd051b56c1694329b6b5add7dd8822d3df1061
Bader Ali <>
2019-04-04 11:37:55 +0200

    hiding social if no pages

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -72,56 +72,59 @@
 </section>
 @endif
 
- <section class="bg-light-gray">
-    <div class="container">
+@if (count($pages) > 0)
+    <section class="bg-light-gray">
+        <div class="container">
 
-        <div class="row">
-            <div class="col-md-7 col-sm-12 col-xs-12 center-col text-center margin-100px-bottom xs-margin-40px-bottom">
-                <div class="position-relative overflow-hidden width-100">
-                    <span class="text-small text-outside-line-full alt-font font-weight-600 text-uppercase">Social media feed</span>
+            <div class="row">
+                <div class="col-md-7 col-sm-12 col-xs-12 center-col text-center margin-100px-bottom xs-margin-40px-bottom">
+                    <div class="position-relative overflow-hidden width-100">
+                        <span class="text-small text-outside-line-full alt-font font-weight-600 text-uppercase">Social media feed</span>
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <div class="row position-relative">
             <div class="row position-relative">
-                <div class="swiper-container swiper-pagination-bottom black-move blog-slider swiper-three-slides">
-                    <div class="swiper-wrapper">
+                <div class="row position-relative">
+                    <div class="swiper-container swiper-pagination-bottom black-move blog-slider swiper-three-slides">
+                        <div class="swiper-wrapper">
 
 
-                        @foreach($pages as $page)
-                            @foreach($page->lastPosts(2) as $post)
-                                <div class="swiper-slide col-md-4 col-sm-4 col-xs-12 blog-post-style5 last-paragraph-no-margin" style="height: 100%">
-                                    <div class="blog-post bg-white box-shadow-light" style="border-radius: 20px;">
-                                        <div class="blog-post-images overflow-hidden">
-                                            <a href="{{$post->url}}">
-                                                <img src="{{$post->image_url}}">
-                                            </a>
-                                                
-                                            <div class="blog-categories bg-white text-uppercase text-extra-small alt-font">
-                                                <a href="#">Facebook</a>
+                            @foreach($pages as $page)
+                                @foreach($page->lastPosts(2) as $post)
+                                    <div class="swiper-slide col-md-4 col-sm-4 col-xs-12 blog-post-style5 last-paragraph-no-margin" style="height: 100%">
+                                        <div class="blog-post bg-white box-shadow-light" style="border-radius: 20px;">
+                                            <div class="blog-post-images overflow-hidden">
+                                                <a href="{{$post->url}}">
+                                                    <img src="{{$post->image_url}}">
+                                                </a>
+                                                    
+                                                <div class="blog-categories bg-white text-uppercase text-extra-small alt-font">
+                                                    <a href="#">Facebook</a>
+                                                </div>
                                             </div>
-                                        </div>
-                                        <div class="post-details inner-match-height padding-40px-all xs-padding-20px-lr xs-padding-30px-tb">
-                                            <div class="blog-hover-color"></div>
-                                            @if ($post->message)
-                                                <a href="{{$post->url}}" class="alt-font post-title text-medium text-extra-dark-gray width-90 display-block md-width-100 margin-5px-bottom cut-text">{{$post->message}}</a>
-                                            @endif
-                                            <div class="author">
-                                                <span class="text-medium-gray text-uppercase text-extra-small display-inline-block">{{$page->name}}</span>
+                                            <div class="post-details inner-match-height padding-40px-all xs-padding-20px-lr xs-padding-30px-tb">
+                                                <div class="blog-hover-color"></div>
+                                                @if ($post->message)
+                                                    <a href="{{$post->url}}" class="alt-font post-title text-medium text-extra-dark-gray width-90 display-block md-width-100 margin-5px-bottom cut-text">{{$post->message}}</a>
+                                                @endif
+                                                <div class="author">
+                                                    <span class="text-medium-gray text-uppercase text-extra-small display-inline-block">{{$page->name}}</span>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
+                                @endforeach
                             @endforeach
-                        @endforeach
-                    </div>                        
-                    <div class="swiper-pagination swiper-pagination-three-slides height-auto"></div>
+                        </div>                        
+                        <div class="swiper-pagination swiper-pagination-three-slides height-auto"></div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</section> 
+    </section> 
+@endif
+
 
 @if (count($events) > 0 && ((bool) getContent('home_show_events')->content))
 <section class="no-padding margin-70px-top">
