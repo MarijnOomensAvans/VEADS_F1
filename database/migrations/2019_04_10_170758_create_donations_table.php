205763e40b442344d20e0b2a1ae6c52f833a9344
Daan van Berkel <info@daanvanberkel.nl>
2019-04-10 17:12:18 +0200

    Donatie migrations

---
+++ database/migrations/2019_04_10_170758_create_donations_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDonationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('donations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->float('amount');
+            $table->string('first_name')->nullable()->default(null);
+            $table->string('last_name')->nullable()->default(null);
+            $table->string('email')->nullable()->default(null);
+            $table->unsignedInteger('event_id')->nullable()->default(null);
+            $table->string('payment_id')->nullable()->default(null); // Mollie payment id
+            $table->dateTime('paid_at')->nullable()->default(null);
+            $table->boolean('refunded')->default(false);
+            $table->timestamps();
+
+            $table->foreign('event_id')->references('id')->on('events');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('donations');
+    }
+}
