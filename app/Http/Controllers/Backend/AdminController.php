92f8c4a934dc599165d80787d4dc4981fa3b5c3e
Bader Ali <>
2019-05-08 16:44:25 +0200

    moved code to service

--- app/Http/Controllers/Backend/AdminController.php
+++ app/Http/Controllers/Backend/AdminController.php
@@ -1,16 +1,9 @@
 <?php
 
 namespace App\Http\Controllers\Backend;
+
 use App\Http\Controllers\Controller;
-use App\Volunteer;
-use Google_Client;
-use Google_Service_Analytics;
-use Google_Service_AnalyticsReporting;
-use Google_Service_AnalyticsReporting_DateRange;
-use Google_Service_AnalyticsReporting_Metric;
-use Google_Service_AnalyticsReporting_ReportRequest;
-use Google_Service_AnalyticsReporting_GetReportsRequest;
-use Storage;
+use App\Services\GoogleService;
 
 class AdminController extends Controller
 {
@@ -18,79 +11,9 @@ class AdminController extends Controller
     	$this->middleware("auth");
     }
 
-    public function index() {
-        $analytics = $this->initializeAnalytics();
-        $response = $this->getReport($analytics);
-        $analyticsData = $this->formatReport($response);
+    public function index(GoogleService $googleService) {
+        $analyticsData = $googleService->getAnalyticsReport();
     	return view('back.admin.index', ['analyticsData' => $analyticsData]);
     }
-
-    function initializeAnalytics(){
-        $KEY_FILE_LOCATION = Storage::disk('local')->path('service-account-credentials.json');
-
-        // Create and configure a new client object.
-        $client = new Google_Client();
-        $client->setApplicationName("Hello Analytics Reporting");
-        $client->setAuthConfig($KEY_FILE_LOCATION);
-        $client->setScopes(['https://www.googleapis.com/auth/analytics.readonly']);
-        $analytics = new Google_Service_AnalyticsReporting($client);
-
-        return $analytics;
-    }
-
-    function getReport($analytics) {
-
-        // Replace with your view ID, for example XXXX.
-        $VIEW_ID = config('google.view_id');
-
-        // Create the DateRange object.
-        $dateRange = new Google_Service_AnalyticsReporting_DateRange();
-        $dateRange->setStartDate("7daysAgo");
-        $dateRange->setEndDate("today");
-
-        // Create the Metrics object.
-        $sessions = new Google_Service_AnalyticsReporting_Metric();
-        $sessions->setExpression("ga:sessions");
-        $sessions->setAlias("sessions");
-
-        // Create the ReportRequest object.
-        $request = new Google_Service_AnalyticsReporting_ReportRequest();
-        $request->setViewId($VIEW_ID);
-        $request->setDateRanges($dateRange);
-        $request->setMetrics(array($sessions));
-
-        $body = new Google_Service_AnalyticsReporting_GetReportsRequest();
-        $body->setReportRequests( array( $request) );
-        return $analytics->reports->batchGet( $body );
-        
-    }
-
-    function formatReport($reports) {
-
-        $analyticsData = [];
-
-        for ( $reportIndex = 0; $reportIndex < count( $reports ); $reportIndex++ ) {
-            $report = $reports[ $reportIndex ];
-            $header = $report->getColumnHeader();
-            $dimensionHeaders = $header->getDimensions();
-            $metricHeaders = $header->getMetricHeader()->getMetricHeaderEntries();
-            $rows = $report->getData()->getRows();
-
-            for ( $rowIndex = 0; $rowIndex < count($rows); $rowIndex++) {
-                $row = $rows[ $rowIndex ];
-                $dimensions = $row->getDimensions();
-                $metrics = $row->getMetrics();
-
-                for ($j = 0; $j < count($metrics); $j++) {
-                    $values = $metrics[$j]->getValues();
-                    for ($k = 0; $k < count($values); $k++) {
-                        $entry = $metricHeaders[$k];
-                        $analyticsData[$entry->getName()]  = $values[$k] ;
-                    }
-                }
-            }
-        }
-
-        return $analyticsData;
-    }
+    
 }
