bdc36fea7ab11105a41d962a085def9547f01cbe
Daan van Berkel <info@daanvanberkel.nl>
2019-05-16 18:41:27 +0200

    Ambassadeur database tabellen toegevoegd

---
+++ database/migrations/2019_05_16_183817_create_ambassadors_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAmbassadorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ambassadors', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('picture_id');
+            $table->string('name');
+            $table->string('company');
+            $table->string('description');
+            $table->boolean('published')->default(true);
+            $table->timestamps();
+
+            $table->foreign('picture_id')->references('id')->on('pictures');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ambassadors');
+    }
+}
