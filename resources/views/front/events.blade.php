80a8ffa1be04eff345e8ff407bf0e0fc2eae9aa2
Bader Ali <>
2019-05-20 11:22:31 +0200

    verlopen events laten zien

--- resources/views/front/events.blade.php
+++ resources/views/front/events.blade.php
@@ -15,22 +15,22 @@
 </section>
 
 @if((bool) getContent('event_show_breadcrumb')->content)
-<section class="padding-20px-tb border-bottom border-color-extra-light-gray" style="visibility: visible">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12 display-table">
-                <div class="display-table-cell vertical-align-middle text-left">
-                    <div class="breadcrumb alt-font text-small no-margin-bottom">
-                        <ul>
-                            <li><a href="/" class="text-medium-gray">Home</a></li>
-                            <li><a href="/event" class="text-medium-gray">{{ getContent('event_title')->content }}</a></li>
-                        </ul>
+    <section class="padding-20px-tb border-bottom border-color-extra-light-gray" style="visibility: visible">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 display-table">
+                    <div class="display-table-cell vertical-align-middle text-left">
+                        <div class="breadcrumb alt-font text-small no-margin-bottom">
+                            <ul>
+                                <li><a href="/" class="text-medium-gray">Home</a></li>
+                                <li><a href="/event" class="text-medium-gray">{{ getContent('event_title')->content }}</a></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
 @endif
 
 @if(!empty(strip_tags($intro = getContent('event_intro')->content)))
@@ -46,17 +46,23 @@
 @endif
 
 <section class="hover-option4 blog-post-style3" style="visibility: visible; animation-name: fadeIn;">
+
     <div class="container">
         <div class="row equalize xs-equalize-auto">
             @foreach ($events as $event)
-            <div class="grid-item col-md-4 col-sm-6 col-xs-12 margin-30px-bottom xs-text-center" style="visibility: visible; animation-name: fadeInUp; height: 542px;">
+            <div class="blog-post-style5 grid-item col-md-4 col-sm-6 col-xs-12 margin-30px-bottom xs-text-center" style="visibility: visible; animation-name: fadeInUp; height: 542px;">
                 <div class="blog-post bg-light-gray inner-match-height">
                     <div class="blog-post-images overflow-hidden position-relative">
                         <a href="/event/{{$event->id}}">
-                                @if(count($event->pictures) > 0)
-                                    <img src="/image/{{ $event->pictures[0]->path }}/{{ $event->pictures[0]->name }}" data-no-retina="">
-                                @endif
-                            </a>
+                            @if(count($event->pictures) > 0)
+                                <img src="/image/{{ $event->pictures[0]->path }}/{{ $event->pictures[0]->name }}" data-no-retina="">
+                            @endif
+                        </a>
+                        @if(new DateTime($event->datetime->end) <  new DateTime())
+                            <div class="blog-categories text-uppercase text-extra-small alt-font" style="background: #EA5B5B; color: white;">
+                                Verkopen
+                            </div>
+                        @endif
                     </div>
                     <div class="post-details padding-40px-all sm-padding-20px-all">
                         <a href="/event/{{$event->id}}" class="alt-font post-title text-medium text-extra-dark-gray width-100 display-block md-width-100 margin-15px-bottom">{{$event->name}}</a>
